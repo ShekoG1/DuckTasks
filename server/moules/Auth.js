308d9a6f324e17dcedbf4f473242337731a4de88
Shekhar Maharaj <shekharmaharaj2905@gmail.com>
2024-06-25 08:29:58 +0200

    Added base code

---
+++ server/moules/Auth.js
@@ -0,0 +1,5 @@
+class Auth {
+    constructor(supabase) {
+        this.supabase = supabase;
+    }
+}
---
+++ server/routes.js
@@ -0,0 +1,9 @@
+// import authRoutes from './endpoints/auth';
+// import userRoutes from './endpoints/users';
+// import postRoutes from './endpoints/posts';
+
+export default [
+  // authRoutes,
+  // userRoutes,
+  // postRoutes
+];
