a6ae80ce7b87a42262f2c1bf0ce73613a4e8c86e
Shekhar Maharaj <shekharmaharaj2905@gmail.com>
2024-06-26 13:45:23 +0200

    Removed server folder from client branch

--- server/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-import routes from './routes';
-
-app.use(express.json());
-app.use('/api', routes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
